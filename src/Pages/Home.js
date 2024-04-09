f904ffc01458e0a846bebc986660134263cf0fa2
vritta <anuhartripathi15@gmail.com>
2024-04-09 15:03:35 +0530

    tagpage component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Header from '../components/Header.js';
+import Pagination from '../components/Pagination.js';
+import Blogs from '../components/Blogs.js';
 const Home = () => {
   return (
-    <div>
-      
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+      <Header/>
+      <Blogs/>
+      <Pagination/>
     </div>
   )
 }
--- src/Pages/TagPage.js
+++ src/Pages/TagPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {useNavigation, useLocation} from 'react-router-dom'
+
+import Header from '../components/Header'
+import Blogs from '../components/Blogs';
+import Pagination from '../components/Pagination';
 
 const TagPage = () => {
+
+  const navigation = useNavigation();
+  const location = useLocation();
+  const tag = location.pathname.split("/").at(-1);
   return (
     <div>
-      
+      <Header/>
+      <div>
+        <button onClick={()=>navigation(-1)}>Back</button>
+        <h2>
+          Blogs Tagged <span>#{tag}</span>
+        </h2>
+      </div>
+      <Blogs/>
+      <Pagination/>
     </div>
   )
 }
